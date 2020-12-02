ea029252697a7c3633309cf252b9794add62c4d2
Eric Schwartz <ericschwartz7@gmail.com>
2020-12-01 22:43:16 -0500

    Untrack files in .gitignore

--- config/initializers/cloudinary.rb
+++
@@ -1,7 +0,0 @@
-Cloudinary.config do |config|
-    config.cloud_name = 'gmg-archive-project'
-    config.api_key = '119581295779122'
-    config.api_secret = 'mgoEaHFgQ1MqFUeOA1C3PFyS-0M'
-    config.secure = true
-    config.cdn_subdomain = true
-  end
