3f47c1f83eae0ed408779efed2f1b6b2dc198733
Eric Schwartz <ericschwartz7@gmail.com>
2021-01-05 00:30:41 -0500

    Fix secret_key_base variable.

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -4,13 +4,13 @@ class JsonWebToken
     class << self
         def encode(payload, exp = 24.hours.from_now)
             raise StandardError.new("payload is nil") unless payload
-            raise StandardError.new("secret key is nil") unless Rails.application.secrets.secret_key_base
+            raise StandardError.new("secret key is nil") unless Rails.application.secret_key_base
             payload[:exp] = exp.to_i
-            JWT.encode(payload, Rails.application.secrets.secret_key_base)
+            JWT.encode(payload, Rails.application.secret_key_base)
         end
     
         def decode(token)
-            body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+            body = JWT.decode(token, Rails.application.secret_key_base)[0]
             HashWithIndifferentAccess.new body
         rescue
             nil
